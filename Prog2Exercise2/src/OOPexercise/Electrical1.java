227d3e5c586d7252a3bcda3a4df21faf08993a76
MAY NG <MAY NG@MayNg>
2021-04-01 14:37:22 +0800

    Exercise 2.1

---
+++ Prog2Exercise2/src/OOPexercise/Electrical1.java
@@ -0,0 +1,23 @@
+package OOPexercise;
+
+public class Electrical1 {
+	String brand = "Canon";
+	String model = "G3010";
+	String scanner = "No scanner for this model. ";
+			
+	void colour() {
+		System.out.println("You have chosen colour printing.");
+	}
+	
+	void black(){
+		System.out.println("The colour you chose is black.");
+	}
+	
+	void directPhone() {
+		System.out.println("Phone is connected.");
+	}
+	
+	void network() {
+		System.out.println("Network is connected.");
+	}
+}
--- Prog2Exercise2/src/OOPexercise/Main.java
+++ Prog2Exercise2/src/OOPexercise/Main.java
@@ -3,25 +3,24 @@ package OOPexercise;
 public class Main {
 
 	public static void main(String[] args) {
-		Printer a = new Printer();
-		System.out.println(a.brand);
-		System.out.println(a.model);
-		System.out.println(a.scanner);
-		a.network();
-		a.directPhone();
-		a.colour();
-		a.black();
+		Electrical1 Canon = new Electrical1();
+		System.out.println("The brand of the printer is " + Canon.brand);
+		System.out.println("The model of the printer is " + Canon.model);
+		System.out.println(Canon.scanner);
+		Canon.network();
+		Canon.directPhone();
+		Canon.colour();
+		Canon.black();
 		System.out.println();
 		
-		AirCond b = new AirCond();
-		System.out.println(b.brand);
-		System.out.println(b.model);
-		System.out.println(b.portable);
-		b.fanMode();
-		b.coolMode();
-		b.timer();
-		b.silentFan();
-
+		Electrical2 Samsung = new Electrical2();
+		System.out.println("The brand of the air conditioner is " + Samsung.brand);
+		System.out.println("The model of the air conditioner is " + Samsung.model);
+		System.out.println(Samsung.portable);
+		Samsung.fanMode();
+		Samsung.coolMode();
+		Samsung.timer();
+		Samsung.silentFan();
 	}
 
 }
